393cc95b5178fd8fc1f15d5741c908b25a3d26b2
Jacob Jenkins <jacob_jenkins@live.com>
2020-09-03 21:29:25 +0100

    wip

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from "rollup-plugin-terser";
 
 export default {
-	input: 'dist/service-worker.js',
+	input: 'dist\\service-worker.js',
 	output: {
-		dir: 'dist',
-		format: 'cjs',
+		file: 'dist\\service-worker.js',
+		format: 'umd',
 		minifyInternalExports: true,
 		compact: true,
 		inlineDynamicImports: true,
